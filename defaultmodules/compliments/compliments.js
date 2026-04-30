e3d2376e69e060c812fd116ae3744161997f337f
Cozma Vlad <vladuzz208@gmail.com>
2026-04-30 15:41:31 +0300

    Added html page

--- defaultmodules/compliments/compliments.js
+++ defaultmodules/compliments/compliments.js
@@ -4,13 +4,13 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
-			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
-			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
-			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
-			"....-01-01": ["Happy new year!"]
-		},
-		updateInterval: 30000,
+		anytime: ["Great to see you!"],
+		morning: ["Good morning!", "Have a great day!", "Sleep well?"],
+		afternoon: ["Good afternoon!", "Hope your day is going well!", "Looking great today!"],
+		evening: ["Good evening!", "Hope your day was wonderful!", "Lovely to see you!"],
+		"....-01-01": ["Happy New Year!"]
+	},
+		updateInterval: 90000,
 		remoteFile: null,
 		remoteFileRefreshInterval: 0,
 		fadeSpeed: 4000,
