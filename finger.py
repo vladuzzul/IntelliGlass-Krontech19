c28ada921c1ce687b3a4d8e8ea4dc549da499c53
Cozma Vlad <vladuzz208@gmail.com>
2026-05-13 18:15:05 +0300

    Rotate the cam 90 degrees

--- finger.py
+++ finger.py
@@ -495,6 +495,7 @@ def main():
                     continue
                 frame_counter += 1
 
+                image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
                 # Flip for selfie view, then convert BGR->RGB for MediaPipe.
                 image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
                 image.flags.writeable = False
